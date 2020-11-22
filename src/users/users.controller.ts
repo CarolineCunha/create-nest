ab30b399e66b8032f245161dcc9cd2dbdeb7acb2
CarolineCunha <caroline.cunha.b@hotmail.com>
2020-11-22 14:33:03 -0300

    texto

---
+++ src/users/users.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body, ValidationPipe, Param, Get } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ReturnUserDto } from './dto/return-user.dto';
+import { UserRole } from './user-roles.enum';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
+    return {
+      user,
+      message: 'Administrador cadastrado com sucesso',
+    };
+  }
+
+
+}
